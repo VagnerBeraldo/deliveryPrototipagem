a615b4dfd96877d6dea14aa221602a25848cc7cc
Vagner Vieira Beraldo <143136085+VagnerBeraldo@users.noreply.github.com>
2025-02-06 15:37:58 -0300

    Update index.js

--- src/componentes/Menu/index.js
+++ src/componentes/Menu/index.js
@@ -1,40 +1,77 @@
+import { useState } from "react";
 import './menu.css';
 
 const Menu = () => {
   const updateMenu = () => {
     const menu = document.getElementById('menu');
-    menu.classList.toggle('open');
+    if (menu) {
+      menu.classList.toggle('open');
+    }
+  };
+
+  const [activeDropdown, setActiveDropdown] = useState(null);
+
+  const toggleDropdown = (menu) => {
+    setActiveDropdown(activeDropdown === menu ? null : menu);
   };
 
   return (
-    
-      <nav id='menu'>
-        <input type='checkbox' id='responsive-menu' onClick={updateMenu} />
-        <label htmlFor='responsive-menu'></label>
-        <ul>
-          <li><a href='/'>Espaço</a></li>
-          <li>
-            <a className='dropdown-arrow' href='/'>Cardápio</a>
-            <ul className='sub-menus'>
-              <li><a href='/'>Entrada</a></li>
-              <li><a href='/'>Prato Principal</a></li>
-              <li><a href='/'>Sobremesa</a></li>
-              <li><a href='/'>Bebidas</a></li>
+    <nav id="menu">
+      <input type="checkbox" id="responsive-menu" onClick={updateMenu} />
+      <label htmlFor="responsive-menu"></label>
+      <ul>
+        <li><a href="/">Espaço</a></li>
+        
+        
+        <li>
+          <a 
+            className="dropdown-arrow" 
+            href="/" 
+            onClick={(e) => {
+              e.preventDefault();
+              toggleDropdown("cardapio");
+            }}
+          >
+            Cardápio
+          </a>
+          {activeDropdown === "cardapio" && (
+            <ul className="sub-menus">
+              <li><a href="/">Entrada</a></li>
+              <li><a href="/">Prato Principal</a></li>
+              <li><a href="/">Sobremesa</a></li>
+              <li><a href="/">Bebidas</a></li>
             </ul>
-          </li>
-          <li><a href='/'>Sobre nós</a></li>
-          <li>
-            <a className='dropdown-arrow' href='/'>Serviços</a>
-            <ul className='sub-menus'>
-              <li><a href='/'>Delivery</a></li>
-              <li><a href='/'>Reserva de Mesa</a></li>
-              <li><a href='/'>Jantar Romântico</a></li>
+          )}
+        </li>
+
+        <li><a href="/">Sobre nós</a></li>
+
+        
+        <li>
+          <a 
+            className="dropdown-arrow" 
+            href="/" 
+            onClick={(e) => {
+              e.preventDefault();
+              toggleDropdown("servicos");
+            }}
+          >
+            Serviços
+          </a>
+          {activeDropdown === "servicos" && (
+            <ul className="sub-menus">
+              <li><a href="/">Delivery</a></li>
+              <li><a href="/">Reserva de Mesa</a></li>
+              <li><a href="/">Jantar Romântico</a></li>
             </ul>
-          </li>
-          <li><a href='/'>Fale conosco</a></li>
-        </ul>
-      </nav>
+          )}
+        </li>
+
+        <li><a href="/">Fale conosco</a></li>
+      </ul>
+    </nav>
   );
 };
 
 export default Menu;
+
